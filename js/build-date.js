92b325484a27bed6322ce212aefeaa81a4b6c5f6
abel <111111>
2020-10-16 01:47:33 +0800

    Site updated: 2020-10-16 01:47:32

--- js/build-date.js
+++ js/build-date.js
@@ -1,5 +1,5 @@
-var build_date = Date.parse(new Date('2018-11-11'));
-var now_date = Date.parse(new Date());
+var build_date = Date.parse(new Date('2018-11-11').toLocaleDateString());
+var now_date = Date.parse(new Date(new Date().toLocaleDateString()));
 var build_days = parseInt((now_date - build_date) / (1000 * 60 * 60 * 24));
 
 $(".build-date-prefix").text(`本站已建立 `)
